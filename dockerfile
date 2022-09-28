8278428b9189b3cb777b06bae3520ecc46952b15
Mansikachhhiya <kachhiyamanshi1542810@gmail.com>
2022-09-28 15:06:15 +0530

    merge me added

--- dockerfile
+++
@@ -1,8 +0,0 @@
-FROM node:14.20.0
-#ENV NODE_ENV=production
-WORKDIR /app
-COPY ["package.json", "package-lock.json*", "./"]
-RUN npm install
-COPY . .
-EXPOSE 8000
-CMD [ "node", "index.js" ]
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const {Router} =require("express");
-const controllers= require("../controllers/index")
-const router = Router();
-
-router.post('/users', controllers.createUser)
-     .get('/users',controllers.getAllUsers)
-     .get('/users/:id',controllers.getUserBYId)
-     .delete("/users/:id",controllers.deleteuser)
-module.exports = router
